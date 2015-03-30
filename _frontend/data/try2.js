1d5b29b71bf51f2e407371931c1b885e90e87094
unknown <patrickmorong@gmail.com>
2015-03-29 18:17:42 -0700

    added hover to graphs

--- _frontend/data/try2.js
+++ _frontend/data/try2.js
@@ -2,7 +2,8 @@ var mglcat = [[1, 98.7], [2, 98.3], [3, 97.8], [4, 98.5]]
 var eposcat = [[1, 86], [2, 88.3], [3, 87.8], [4, 88.5]]
 var maincat = [[1, 95.7], [2, 95.3], [3, 94.8], [4, 95.5]]
 
-var catovdata = [{ data:mglcat, label:"Midwest & Great Lakes", lines:{show:true}}
+
+var catovdata = [{ data:mglcat, label:"Midwest & Great Lakes", lines:{show:true}, points:{show:true}}
         ,{ data:eposcat, label:"Tempe EPOS", lines:{show:true}, points:{show:true}}
 		,{ data:maincat, label:"Tempe Maintenance", lines:{show:true}, points:{show:true}}
 		];
@@ -15,8 +16,20 @@ var options = {
             axisLabelUseCanvas: true,
             axisLabelFontSizePixels: 20,
             axisLabelFontFamily: 'Arial'
+		},
+		yaxis: {
+            axisLabel: '% answered in less than 3 min',
+            axisLabelUseCanvas: true
+        },
+		grid: {
+			show: true,
+			hoverable: true
+		},
+		tooltip: true,
+		tooltipOpts: {
+			content: "%y"
 		}
 	
 }
-		
+
 $(document).ready(function () {$.plot($("#catovline"), catovdata, options);});	 
